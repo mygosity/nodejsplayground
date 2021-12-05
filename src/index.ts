6edfca0f46bb84756451211c07b0b86207bf3e66
Michael.Lee <mygosity@gmail.com>
2021-12-05 16:33:38 +1100

    added lesson from comparing strings instead of integers

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,35 @@ function readFile(path: string) {
 function handleInput001() {
 	const inputText = readFile(rootFilePath + '\\inputs\\mygosity\\input001.txt');
 	const list = inputText.split('\n').map((e) => parseInt(e));
+
+	const list2 = inputText.split('\n');
+	const diffs = [];
+	for (let i = 0; i < list2.length; ++i) {
+		//@ts-ignore
+		if (list2[i] != list[i]) {
+			diffs.push(list2[i]);
+		}
+	}
+
 	let answer = 0;
 	for (let i = 1; i < list.length; ++i) {
 		if (list[i] > list[i - 1]) {
 			answer++;
+			if (list2[i] <= list2[i - 1]) {
+				diffs.push([i, list2[i], list2[i - 1]]);
+			}
 		}
 	}
+	console.log({
+		diffs,
+		list1len: list.length,
+		list2len: list2.length,
+	});
+	console.log('test string:: ', '1015' > '995');
 	console.log({ list, answer });
 }
 
 function main() {
-	handleInput001();
+	// handleInput001();
 }
 main();
