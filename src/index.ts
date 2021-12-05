c5e3665c1c3a89da22bd760ecd9469fa4ad31059
Michael.Lee <mygosity@gmail.com>
2021-12-05 16:26:02 +1100

    submitted answer and input for 001

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("hello world");
+import fs from 'fs';
+import path from 'path';
+
+const rootFilePath = path.resolve(__dirname, '../');
+
+function readFile(path: string) {
+	return fs.readFileSync(path).toString();
+}
+
+function handleInput001() {
+	const inputText = readFile(rootFilePath + '\\inputs\\mygosity\\input001.txt');
+	const list = inputText.split('\n').map((e) => parseInt(e));
+	let answer = 0;
+	for (let i = 1; i < list.length; ++i) {
+		if (list[i] > list[i - 1]) {
+			answer++;
+		}
+	}
+	console.log({ list, answer });
+}
+
+function main() {
+	handleInput001();
+}
+main();
